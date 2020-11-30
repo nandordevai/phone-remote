f4abdab7ea9ec0bb82ed434c74f25bd959f24ab4
Nandor Devai <mail@nandordevai.net>
2020-11-30 16:59:09 +0100

    giving up iOS security too strict for dev

--- assets/mobile.js
+++ assets/mobile.js
@@ -1,15 +1,17 @@
-// const ws = new WebSocket('wss://10.0.1.78:3000'); // FIXME
-// ws.onopen = () => {
-//     ws.send('mobile connected');
-// };
+const ws = new WebSocket('wss://10.0.1.78:3000'); // FIXME
+ws.onopen = () => {
+    ws.send('mobile connected');
+};
 
 function handleMotionEvent(event) {
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
+    ws.send(x, y, z);
 }
 
 window.addEventListener('devicemotion', handleMotionEvent, true);
-document.querySelector('#request').addEventListener('click', () => {
-    DeviceOrientationEvent.requestPermission();
+const btn = document.querySelector('#request');
+btn.addEventListener('click', () => {
+    DeviceOrientationEvent.requestPermission().then(() => { btn.setAttribute('disabled', 'disabled'); });
 });
